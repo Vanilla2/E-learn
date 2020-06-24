9a27eccf9fdcbbc9cb8792f64fcd61b765460958
Vanilla <solmarians05@gmail.com>
2020-06-24 23:56:34 +0300

    Updates

---
+++ precache-manifest.a84cdd7694086574243ef6d00af9bda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75dee28fd3f01e038feb6469a13e6eb7",
+    "url": "/E-learn/index.html"
+  },
+  {
+    "revision": "e4029c7d7a30a3f4c697",
+    "url": "/E-learn/static/css/main.6d3e28cd.chunk.css"
+  },
+  {
+    "revision": "31ce5716cfc898b0d396",
+    "url": "/E-learn/static/js/2.d8baf892.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/E-learn/static/js/2.d8baf892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4029c7d7a30a3f4c697",
+    "url": "/E-learn/static/js/main.bc0e7397.chunk.js"
+  },
+  {
+    "revision": "98e8985ae581c150ddd9",
+    "url": "/E-learn/static/js/runtime-main.472708dd.js"
+  }
+]);
